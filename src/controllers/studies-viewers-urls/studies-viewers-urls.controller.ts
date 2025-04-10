85b8298f1eabae2b56daf16b6754888432c37fc7
Nahuel Alderete <nahuel.ald@gmail.com>
2025-04-10 20:26:34 -0300

    agregada validación de número de acceso y obtención de datos de estudio en el controlador de URLs de visualizadores

--- src/controllers/studies-viewers-urls/studies-viewers-urls.controller.ts
+++ src/controllers/studies-viewers-urls/studies-viewers-urls.controller.ts
@@ -1,7 +1,15 @@
 import { Request, Response } from "express";
 import { logger } from "../../loggin-service";
+import { ORTHANC_MAPPING_INFO, OrthancConnector } from "../../orthanc-connection";
+
+const ACCESSION_NUMBER_KEY = "0008,0050";
+const STUDY_UID_KEY = "0020,000d";
 
 export class StudiesViewersUrlsController {
+    private DICOMConnector: OrthancConnector;
+
+    constructor() { this.DICOMConnector = new OrthancConnector(); }
+
     /**
      * @description
      * @param {Request} request
@@ -12,10 +20,19 @@ export class StudiesViewersUrlsController {
         try {
             const requestAccessionNumber = request.params?.accessionNumber;
 
+            if (!requestAccessionNumber) { response.status(400).json({ message: "accession number required" }); return; }
+
+            const accNumberKey = ORTHANC_MAPPING_INFO[ACCESSION_NUMBER_KEY];
+            const studyUIDKey = ORTHANC_MAPPING_INFO[STUDY_UID_KEY];
+
+            const dataEstudio = (await this.DICOMConnector.getStudies()).find(x => x[accNumberKey.mappedName || accNumberKey.name] === requestAccessionNumber);
+
+            if (!dataEstudio) { response.status(404).json(null); return; }
+
             const returnObj = {
                 accessionNumber: requestAccessionNumber,
-                ohif: `${process.env.VIEWER_FRONTEND_URL}/${process.env.OHIF_VIEWER.replace("{id}", requestAccessionNumber)}`,
-                stone: `${process.env.VIEWER_FRONTEND_URL}/${process.env.STONE_VIEWER.replace("{id}", requestAccessionNumber)}`,
+                ohif: `${process.env.VIEWER_FRONTEND_URL}/${process.env.OHIF_VIEWER.replace("{id}", dataEstudio['ID'])}`,
+                stone: `${process.env.VIEWER_FRONTEND_URL}/${process.env.STONE_VIEWER.replace("{id}", dataEstudio[studyUIDKey.mappedName || studyUIDKey.name])}`,
 
             }
 
